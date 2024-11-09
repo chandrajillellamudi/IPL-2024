fb36372838526807383a51b1be258c9394d3e03a
chandrajillellamudi <chandrajillellamudi0@gmail.com>
2024-11-09 19:06:50 +0530

    ipl 2024 home page done.

---
+++ scorecards-array.js
@@ -0,0 +1,248 @@
+const scoreCards = [
+    {
+        result_section: {
+            "title": "FINAL",
+            "text": "KOLKATA KNIGHT RIDERS WON BY 8 WICKETS(WINNERS)"
+        },
+        scorecard_section: {
+            venue: "MA Chidambaram Stadium, Chennai",
+            date: "May,SUN 26,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Kolkata Knight Riders",
+                    "team_short": "KKR",
+                    "score": "114/2",
+                    "overs": "10.3"
+                },
+                {
+                    "team": "Sunrisers Hyderabad",
+                    "team_short": "SRH",
+                    "score": "113/10",
+                    "overs": "18.3"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "QUALIFIER-2",
+            "text": "SUNRISERS HYDERABAD WON BY 36 RUNS(QUALIFIED)"
+        },
+        scorecard_section: {
+            venue: "MA Chidambaram Stadium, Chennai",
+            date: "May,FRI 24,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Sunrisers Hyderabad",
+                    "team_short": "SRH",
+                    "score": "175/9",
+                    "overs": "20.0"
+                },
+                {
+                    "team": "Rajasthan Royals",
+                    "team_short": "RR",
+                    "score": "139/10",
+                    "overs": "20.0"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "ELIMINATOR",
+            "text": "RAJASTHAN ROYALS WON BY 4 WICKETS(QUALIFIED)"
+        },
+        scorecard_section: {
+            venue: "Narendra Modi Stadium, Ahmedabad",
+            date: "May,WED 22,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Rajasthan Royals",
+                    "team_short": "RR",
+                    "score": "174/6",
+                    "overs": "19.0"
+                },
+                {
+                    "team": "Royal Challengers Bengaluru",
+                    "team_short": "RCB",
+                    "score": "172/8",
+                    "overs": "20.0"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "QUALIFIER-1",
+            "text": "KOLAKATA KNIGHT RIDERS WON BY 8 WICKETS(QUALIFIED)"
+        },
+        scorecard_section: {
+            venue: "Narendra Modi Stadium, Ahmedabad",
+            date: "May,TUE 21,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Kolkata Knight Riders",
+                    "team_short": "KKR",
+                    "score": "164/2",
+                    "overs": "13.4"
+                },
+                {
+                    "team": "Sunrisers Hyderabad",
+                    "team_short": "SRH",
+                    "score": "159/10",
+                    "overs": "19.3"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "MATCH-70",
+            "text": "KOLKATA KNIGHT RIDERS WON BY 4 WICKETS"
+        },
+        scorecard_section: {
+            venue: "Barsapara Cricket Stadium, Guwahati",
+            date: "May,SUN 19,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Rajasthan Royals",
+                    "team_short":"RR",
+                    "score": "190/6",
+                    "overs": "20.0"
+                    
+                },
+                {
+                    "team": "Kolkata Knight Riders",
+                    "team_short":"KKR",
+                    "score": "194/6",
+                    "overs": "20.0",
+                    
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "MATCH-69",
+            "text": "SUNRISERS HYDERABAD WON BY 4 WICKETS"
+        },
+        scorecard_section: {
+            venue: "Rajiv Gandhi International Stadium, Hyderabad",
+            date: "May,SUN 19,3:30 pm IST",
+            teams: [
+                {
+                    "team": "Sunrisers Hyderabad",
+                    "team_short": "SRH",
+                    "score": "215/6",
+                    "overs": "19.1"
+                },
+                {
+                    "team": "Punjab Kings",
+                    "team_short": "PK",
+                    "score": "214/5",
+                    "overs": "20.0"
+                }
+            ]
+        },
+    },
+   
+    {
+        result_section: {
+            "title": "MATCH-68",
+            "text": "ROYAL CHALLENGERS BENGALURU WON BY 27 RUNS"
+        },
+        scorecard_section: {
+            venue: "M Chinnaswamy Stadium, Bengaluru",
+            date: "May,SAT 18,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Royal Challengers Bengaluru",
+                    "team_short": "RCB",
+                    "score": "218/5",
+                    "overs": "20.0"
+                },
+                {
+                    "team": "Chennai Super Kings",
+                    "team_short": "CSK",
+                    "score": "191/7",
+                    "overs": "20.0"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "MATCH-67",
+            "text": "LUCKNOW SUPER GIANTS WON BY 16 RUNS"
+        },
+        scorecard_section: {
+            venue: "Wankhede Stadium, Mumbai",
+            date: "May,FRI 17,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Mumbai Indains",
+                    "team_short": "MI",
+                    "score": "196/6",
+                    "overs": "20.0"
+                },
+                {
+                    "team": "Lucknow Super Giants",
+                    "team_short": "LSG",
+                    "score": "214/6",
+                    "overs": "20.0"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "MATCH-66",
+            "text": "GUJARAT TITANS WON BY 9 RUNS"
+        },
+        scorecard_section: {
+            venue: "Rajiv Gandhi International Stadium, Hyderabad",
+            date: "May,THU 16,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Sunrisers Hyderabad",
+                    "team_short": "SRH",
+                    "score": "190/6",
+                    "overs": "20.0"
+                },
+                {
+                    "team": "Gujarat Titans",
+                    "team_short": "GT",
+                    "score": "200/6",
+                    "overs": "20.0"
+                }
+            ]
+        },
+    },
+    {
+        result_section: {
+            "title": "MATCH-65",
+            "text": "PUNJAB KINGS WON BY 5 WICKETS"
+        },
+        scorecard_section: {
+            venue: "Barsapara Cricket Stadium, Guwahati",
+            date: "May,WED 15,7:30 pm IST",
+            teams: [
+                {
+                    "team": "Rajasthan Royals",
+                    "team_short": "RR",
+                    "score": "144/9",
+                    "overs": "20.0"
+                },
+                {
+                    "team": "Punjab Kings",
+                    "team_short": "PK",
+                    "score": "145/5",
+                    "overs": "18.5"
+                }
+            ]
+        },
+    }
+    
+    
+
+]
