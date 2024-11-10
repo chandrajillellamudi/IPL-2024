60796977728aa70df4ce0d30ca3b5a77479350c8
chandrajillellamudi <chandrajillellamudi0@gmail.com>
2024-11-10 22:01:04 +0530

    teams updated

---
+++ teams-array.js
@@ -0,0 +1,64 @@
+const teams=[
+    {
+        "title" :"Chennai Super Kings",
+        "image" :"https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Chennai_Super_Kings_Logo.svg/1200px-Chennai_Super_Kings_Logo.svg.png",
+        "color":"yellow",
+        "tcolor":"black",
+    },
+    {
+        "title" :"Delhi Capitals",
+        "image" :"https://upload.wikimedia.org/wikipedia/en/2/2f/Delhi_Capitals.svg",
+        "color":"brown",
+       
+    },
+    {
+        "title" :"Gujarat Titans",
+        "image" :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGc2I3jTLVIuptfr9pR7bUucMJJssNuuT8rg&s",
+        "color":"skyblue",
+       
+    },
+    {
+        "title" :"Kolkata Knight Riders",
+        "image" :"https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Kolkata_Knight_Riders_Logo.svg/800px-Kolkata_Knight_Riders_Logo.svg.png",
+        "color":"gold",
+       
+    },
+    {
+        "title" :"Lucknow Super Giants",
+        "image" :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuw_KC2fyhE31K-XtK874zMtvsFIZT-0K2cw&s",
+        "color":"white",
+       
+    },
+    {
+        "title" :"Mumbai Indians",
+        "image" :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZGEZ5pBr-bX7JIVK7-kJ4wOoMMWg8F4aBlw&s",
+        "color":"blue"
+      
+    },
+    {
+        "title" :"Punjab Kings",
+        "image" :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZbZBFeDoFMDSQFWKyu20ZXxhlBu6fJDpGDg&s",
+        "color":"red",
+       
+    },
+    {
+        "title" :"Rajasthan Royals",
+        "image" :"https://banner2.cleanpng.com/20180528/gyy/avpfqkjep.webp",
+        "color":"pink",
+       
+    },
+    {
+        "title" :"Royal Challengers Bangalore",
+        "image" :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzcfFd-wQLEQF1p6PeUSzFvR2w_MxtjDjKww&s",
+        "color":"tan",
+       
+    },
+    {
+        "title" :"Sunrisers Hyderabad",
+        "image" :"https://i.pinimg.com/originals/b7/7d/f5/b77df59b1a5f0f209b94a0148ddb02d5.jpg",
+        "color":"orange",
+       
+    },
+
+]
+    
